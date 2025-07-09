f39409e97711ab35de4a88a63ef076df24be8001
Benedikt Schesch <scheschb@amazon.co.uk>
2025-07-09 21:00:18 +0100

    Added latest data points

--- dataset_build_scripts/build_dataset_python.sh
+++ dataset_build_scripts/build_dataset_python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script to collect all merges on a small dataset
 
-dataset_build_scripts/build_dataset.sh input_data/repos_reaper_Python_0_1200.csv merges/repos_reaper_1000 "$@" --max_num_merges 1 --language python
+dataset_build_scripts/build_dataset.sh input_data/repos_reaper_Python_0_1200.csv merges/repos_reaper_python "$@" --max_num_merges 1 --language python
