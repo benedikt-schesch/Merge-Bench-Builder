3447fdae6d280a33ff574e284f064cc28fe4cf01
Benedikt Schesch <scheschb@amazon.co.uk>
2025-07-11 08:29:10 +0100

    Reduced num merges

--- dataset_build_scripts/build_dataset_php.sh
+++ dataset_build_scripts/build_dataset_php.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script to collect all merges on a PHP dataset
 
-dataset_build_scripts/build_dataset.sh input_data/repos_reaper_PHP_0_1200.csv merges/repos_reaper_php "$@" --max_num_merges 100 --language php --test_size 1.0
+dataset_build_scripts/build_dataset.sh input_data/repos_reaper_PHP_0_1200.csv merges/repos_reaper_php "$@" --max_num_merges 50 --language php --test_size 1.0
