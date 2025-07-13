f24fb22802dffc74271dd330049966bd28f7fd8b
Benedikt Schesch <scheschb@amazon.co.uk>
2025-07-13 16:55:14 +0100

    Updated java test dataset

--- dataset_build_scripts/build_dataset_reaper_java_1000_1200.sh
+++ dataset_build_scripts/build_dataset_reaper_java_1000_1200.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Script to build dataset for Java repositories 1000-1200 from Reaper
-# This script runs all three steps: get conflicts, metrics, and build dataset
 
-./dataset_build_scripts/build_dataset.sh input_data/repos_reaper_Java_1000_1200.csv output_data/reaper_java_1000_1200 -g -m -b --language java
+./dataset_build_scripts/build_dataset.sh input_data/repos_reaper_Java_1000_1200.csv merges/reaper_java_1000_1200 -g -m -b --language java
