c2f4aef8b065e3816ca822041abe2aaf99ecac0e
Benedikt Schesch <scheschb@amazon.co.uk>
2025-07-16 14:46:39 +0100

    Reduced rust size

--- dataset_build_scripts/build_dataset_rust.sh
+++ dataset_build_scripts/build_dataset_rust.sh
@@ -2,4 +2,4 @@
 # Script to collect all merges on Rust dataset from GitHub
 
 # Pass through all command line arguments to build_dataset.sh
-dataset_build_scripts/build_dataset.sh input_data/repos_github_Rust_0_1000.csv merges/repos_github_rust "$@" --max_num_merges 7 --language rust --test_size 1.0
+dataset_build_scripts/build_dataset.sh input_data/repos_github_Rust_0_1000.csv merges/repos_github_rust "$@" --max_num_merges 6 --language rust --test_size 1.0
